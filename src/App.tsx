f8b9323b0e5bfb0225d550ad8e58e29d0cd1dcdf
taejune park <ptj1554@gmail.com>
2020-04-07 22:59:08 +0900

    방콕 첼린지 개발 시작

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components"
+import MainPage from "./pages/MainPage"
+
+export default function App() {
+  return (
+    <Container >
+      <MainPage />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+
+`
+
---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+import { device } from "../../asset/mediaSize"
+
+export default function MainPage() {
+    return (
+        <Container>asd</Container>
+    )
+}
+
+const Container = styled.div`
+ 
+`
+
