f012e12b54bd999049fa3d81a3552d83f78284a6
taejune park <ptj1554@gmail.com>
2020-04-10 16:51:10 +0900

    Updates

---
+++ precache-manifest.8c3aff0a0932587260bb581dc1edcb16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaee6d050c49a458ab27ca81b74fe2fa",
+    "url": "/TaeJuneP/bangkok_challenge/index.html"
+  },
+  {
+    "revision": "21f01113008a183d0d06",
+    "url": "/TaeJuneP/bangkok_challenge/static/js/2.682e8b36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TaeJuneP/bangkok_challenge/static/js/2.682e8b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5949f16e586f53ddc93b",
+    "url": "/TaeJuneP/bangkok_challenge/static/js/main.f7ffb950.chunk.js"
+  },
+  {
+    "revision": "d0c2beafc07a7a8e0fb6",
+    "url": "/TaeJuneP/bangkok_challenge/static/js/runtime-main.e2175c64.js"
+  },
+  {
+    "revision": "d1cf2a135e9c3f3a0ffe350e5f27d88d",
+    "url": "/TaeJuneP/bangkok_challenge/static/media/logo.d1cf2a13.png"
+  },
+  {
+    "revision": "29602a9c12602fd5ca886818e6cd025a",
+    "url": "/TaeJuneP/bangkok_challenge/static/media/main_logo.29602a9c.png"
+  }
+]);
